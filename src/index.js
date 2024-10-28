ad8a19aee1034abeb92699b40e15db18a37f1f5d
Shaheer Shukur <shaheershukur@gmail.com>
2024-10-28 09:11:20 +0400

    fix: cleans up

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// import { x, rpcErrors } from "../dist/rpc-errors";
-
-// console.log(">>>x", x);
-// console.log(">>>rpcErrors", rpcErrors);
-
-import { JsonRpcEngine } from "../dist/json-rpc-engine/index";
-
-console.log(">>>JsonRpcEngine", JsonRpcEngine);
