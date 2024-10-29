1cf3ddfbeeaebd433793b8f78478079f8f6be8ce
Shaheer Shukur <shaheershukur@gmail.com>
2024-10-29 10:13:04 +0400

    feat: adds jest babel config

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "js"],
+};
+
+export default config;
