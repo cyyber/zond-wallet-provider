4cf17f8fd738850732b03caad9d0be5e23ff07bc
Shaheer Shukur <shaheershukur@gmail.com>
2024-10-29 16:41:56 +0400

    fix: adds textencoder to jest config setup

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   moduleFileExtensions: ["ts", "js"],
+  setupFiles: ["<rootDir>/jest.setup.ts"],
 };
 
 export default config;
